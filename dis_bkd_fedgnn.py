a55e3f77b7a1b1587c1bcf9751c0186d2ebe5c64
xujing1994 <jingxu.buaa@gmail.com>
2022-09-07 16:43:08 +0200

    save path changed

--- dis_bkd_fedgnn.py
+++ dis_bkd_fedgnn.py
@@ -136,8 +136,8 @@ if __name__ == '__main__':
                 tmp_acc = gnn_evaluate_accuracy_v2(attack_loader_list[j], client[i].model)
                 print('Client %d with local trigger %d: %.3f'%(i, j, tmp_acc))
                 att_list.append(tmp_acc)
-            if not args.filename == "" and (i == 0 or i == args.num_mali):
-                save_path = os.path.join(args.filename, config['model'] + '_' + args.dataset + \
+            if not args.filename == "":
+                save_path = os.path.join(args.filename, args.seed, config['model'] + '_' + args.dataset + \
                     '_%d_%d_%.2f_%.2f_%.2f'%(args.num_workers, args.num_mali, args.frac_of_avg, args.poisoning_intensity, args.density) + '_%d.txt'%i)
                 path = os.path.split(save_path)[0]
                 isExist = os.path.exists(path)
@@ -184,7 +184,7 @@ if __name__ == '__main__':
         test_acc = gnn_evaluate_accuracy_v2(client[0].test_iter, client[0].model)
         print('Global Test Acc: %.3f'%test_acc)
         if not args.filename == "":
-            save_path = os.path.join(args.filename, MODEL_NAME + '_' + args.dataset + '_%d_%d_%.2f_%.2f_%.2f'\
+            save_path = os.path.join(args.filename, args.seed, MODEL_NAME + '_' + args.dataset + '_%d_%d_%.2f_%.2f_%.2f'\
                         %(args.num_workers, args.num_mali, args.frac_of_avg, args.poisoning_intensity, args.density) + '_global_test.txt')
             path = os.path.split(save_path)[0]
             isExist = os.path.exists(path)
@@ -206,7 +206,7 @@ if __name__ == '__main__':
                 local_att_acc.append(tmp_acc)
 
             if not args.filename == "":
-                save_path = os.path.join(args.filename, MODEL_NAME + '_' + args.dataset + '_%d_%d_%.2f_%.2f_%.2f'\
+                save_path = os.path.join(args.filename, args.seed, MODEL_NAME + '_' + args.dataset + '_%d_%d_%.2f_%.2f_%.2f'\
                            %(args.num_workers, args.num_mali, args.frac_of_avg, args.poisoning_intensity, args.density) + '_global_attack.txt')
                 path = os.path.split(save_path)[0]
                 isExist = os.path.exists(path)
